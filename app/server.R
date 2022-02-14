fad55ee0fff2a659944cd029e27d8fa794e6fd8c
ozi543 <ozi.pvt@gmail.com>
2022-02-14 13:33:39 +0530

    changed URL

--- app/server.R
+++ app/server.R
@@ -37,7 +37,7 @@ observe({
     if (is.null(user_token())) {
       
       output$main <- renderUI({
-        url <- get_authorization_url(app, "http://127.0.0.1:8080/")
+        url <- get_authorization_url(app, "https://projectdataliteracy.net")
         twitAuth_UI('lv0', url = url)
       })
       
